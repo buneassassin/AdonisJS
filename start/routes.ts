1e243557e65f4d8bb2e48aff129deb79478743ce
bune <bune_assassin@hotmail.com>
2024-10-12 17:42:08 -0600

    mas

--- start/routes.ts
+++ start/routes.ts
@@ -11,9 +11,13 @@ import router from '@adonisjs/core/services/router'
 import TablasMultiplicarsController from '#controllers/tablas_multiplicars_controller'
 import AhorcadosController from '#controllers/ahorcados_controller'
 import PersonasController from '#controllers/personas_controller'
+const AuthController = () => import('#controllers/auth_controller')
+import { middleware } from './kernel.js'
+
 // import auth from '@adonisjs/auth/services/main'
-import AuthController from '#controllers/auth_controller'
 import MusicaDbsController from '#controllers/musica_dbs_controller'
+
+router.group(() => {
 
 // router.get('/tablas/{num1?}/{num2?}/{fibonacci?}', [TablasMultiplicarsController,'index'])
 router.get('/tablas/:num1?/:num2?/:fibonacci?', [TablasMultiplicarsController,'index'])
@@ -23,7 +27,16 @@ router.post('/personas', [PersonasController,'store'])
 router.post('/ahorcado/:palabra?/:intentos?/:p?', [AhorcadosController,'update']);
 router.get('/ahorcado', [AhorcadosController,'show']);
 
-router.get('/registrar', [AuthController,'register']);
+router.get('/ver', [MusicaDbsController,'index']);
 
-router.get('/registrar', [MusicaDbsController,'index']);
+})
+.use(
+  middleware.auth({
+    guards: ['api'],
+  })
+) // Aquí protegemos el grupo de rutas con el middleware de autenticación
+
+router.post('/register', [AuthController, 'register']).as('auth.register')
+router.post('/login', [AuthController, 'login']).as('auth.login')
+router.get('/me', [AuthController, 'me']).as('auth.me')
 
